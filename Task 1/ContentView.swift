6a07ffab03c90b20a9b0a283ed48cd902a70975a
OgabekDev <ogabekdev@gmail.com>
2022-12-01 17:15:08 +0500

    All Done

--- Task 1/ContentView.swift
+++ Task 1/ContentView.swift
@@ -8,19 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State var welcome = "str_welcome".localized()
+        
+        var body: some View {
+            VStack{
+                VStack{
+                    Text(welcome)
+                }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+                HStack(spacing:0){
+                    VStack{
+                        Button(action: {
+                            Bundle.setLanguage(lang: "en")
+                            welcome = "str_welcome".localized()
+                        },label:{
+                            Text("English").foregroundColor(Color.white)
+                                .frame(maxWidth:.infinity, maxHeight: .infinity).background(Color.red)
+                        })
+                    }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                    
+                    VStack{
+                        Button(action: {
+                            Bundle.setLanguage(lang: "ru")
+                            welcome = "str_welcome".localized()
+                        },label:{
+                            Text("Russian").foregroundColor(Color.white)
+                                .frame(maxWidth:.infinity, maxHeight: .infinity).background(Color.blue)
+                        })
+                    }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                    
+                    VStack{
+                        Button(action: {
+                            Bundle.setLanguage(lang: "uz")
+                            welcome = "str_welcome".localized()
+                        },label:{
+                            Text("Uzbek").foregroundColor(Color.white)
+                                .frame(maxWidth:.infinity, maxHeight: .infinity).background(Color.green)
+                        })
+                    }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                    
+                    VStack{
+                        Button(action: {
+                            Bundle.setLanguage(lang: "fr")
+                            welcome = "str_welcome".localized()
+                        },label:{
+                            Text("Franch").foregroundColor(Color.white)
+                                .frame(maxWidth:.infinity, maxHeight: .infinity).background(Color.purple)
+                        })
+                    }.frame(maxWidth: .infinity, maxHeight: .infinity)
+                }.frame(maxWidth: .infinity, maxHeight: 70)
+            }
         }
-        .padding()
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environment(\.locale, .init(identifier: "uz"))
     }
 }
